b48dfb5571002a1f9d8019fd37cd68a4e26f0987
Ethan Li <fourdimentional31@gmail.com>
2026-08-30 20:58:23 -0400

    Update main.py

--- firmware/main.py
+++ firmware/main.py
@@ -1,6 +1,6 @@
 from js import window, console # type: ignore
 from pyscript import document, web # type: ignore
-import asyncio
+import asyncio, time
 
 connected_status = web.page['connect']
 printer_connected = False
@@ -17,11 +17,7 @@ async def send_to_printer(message):
     message = str(message)
     global printer_connected, connected_status
     if printer_connected:
-        if not message == 'Heartbeat':
-            # Disable Client Debugging
-            #display("CLIENT:" + message)
-            console.log('CLIENT:'+str(message))
-            pass
+        console.log('CLIENT:'+str(message))
     try:
         await window.writeSerial(message)
     except Exception as e:
@@ -49,11 +45,12 @@ async def connect(event=None):
         await send_command('import os, sys')
         await send_command('device_info = os.uname()')
         await send_command('print(sys.version, device_info.machine)')
+        await speed_test()
 
 async def check_if_disconnected():
     global printer_connected, lastmessage, connected_status
     try:
-        await send_to_printer('Heartbeat')
+        await send_to_printer('ECHO:Heartbeat')
     except Exception as e:
         pass
     await asyncio.sleep(0.1)
@@ -79,6 +76,29 @@ async def send_button(event=None):
 
 async def send_command(command):
     return await send_to_printer('serverexecutable:'+command)
+
+async def speed_test():
+    global lastmessage
+    times = []
+    payload = ''
+    payload_size = 2048
+    iteration_count = 100
+    console.log(f'Start Speed Test - Config: Payload size: {payload_size}, Iteration Count: {iteration_count}')
+    for p in range(payload_size):
+        payload = payload + '0'
+    for i in range(iteration_count):
+        deltatime = time.ticks_us()
+        await send_to_printer('ECHO:'+payload+str(i))
+        while not lastmessage == 'ECHO:'+payload+str(i):
+            await asyncio.sleep(0.01)
+        times.append(time.ticks_diff(time.ticks_us(), deltatime))
+    count = 0
+    for c in range(len(times)):
+        count = count + times[c]/1000000
+    count = count/len(times)
+    console.log(f'Connection Speed: {round((2*payload_size/count)/1000, 2)} kB/s')
+    display(f'Connection Speed: {round((2*payload_size/count)/1000, 2)} kB/s')
+
 
 document.querySelector("#connect").onclick = connect
 document.querySelector("#send").onclick = send_button
