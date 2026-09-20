6703b9fec9bb0c6442d2b53fe1d401ad1c71d0e4
Ethan Li <fourdimentional31@gmail.com>
2026-09-20 18:29:23 -0400

    Add Endbutton support

--- firmware/printerdriver.py
+++ firmware/printerdriver.py
@@ -101,7 +101,13 @@ class StepperMotor:
         else:
             return
 
+
 class EndButton:
+    """
+    # End Button
+    3d printer End Button, takes 1 pin location as input: `pin`
+    """
+
     def __init__(self, pin):
         self.pin_obj = machine.Pin(pin, machine.Pin.IN, machine.Pin.PULL_UP)
 
@@ -112,6 +118,7 @@ class EndButton:
             return True
         else:
             raise MachineError("Button Indeterminate state")
+
 
 class ThreeAxisControlSystem:
     """
@@ -191,11 +198,17 @@ class ThreeAxisControlSystem:
         if time:
             asyncio.run(self.asyncmovetopoint(point, time))
 
+
 x_motor = StepperMotor(enable_pin=0, step_pin=1, dir_pin=2)
 y_motor = StepperMotor(enable_pin=3, step_pin=4, dir_pin=5)
 z_motor = StepperMotor(enable_pin=6, step_pin=7, dir_pin=8)
+x_button = EndButton(pin=21)
+y_button = EndButton(pin=22)
+z_button = EndButton(pin=26)
 
-control_system = ThreeAxisControlSystem(x_motor, y_motor, z_motor)
+control_system = ThreeAxisControlSystem(
+    x_motor, y_motor, z_motor, x_limit=x_button, y_limit=y_button, z_limit=z_button
+)
 
 control_system.moveto(x=75, y=75, z=0, time=0.25)
 control_system.moveto(x=0, y=0, z=0, time=0.25)
